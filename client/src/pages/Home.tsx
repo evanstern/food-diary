d2984a9191ea8a72e4dc90409f232606fcf42aef
Evan Stern <evanstern@machineservant-llc.com>
2019-10-07 09:58:41 -0400

    Make home page a little nicer

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import { Layout } from 'components/Layout';
+import { Header, Divider } from 'semantic-ui-react';
+import auth from 'utils/auth0';
+import { Link } from 'react-router-dom';
 
 export const Home: React.FC = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <Header as="h1" textAlign="center">
+        Welcome to Food Diary
+      </Header>
+      <Divider horizontal>Track your food</Divider>
+      {auth.isAuthenticated() ? (
+        <Header as="h3" textAlign="center">
+          <Link to="/food-diary/">Get started!</Link>
+        </Header>
+      ) : (
+        <Header as="h3" textAlign="center">
+          Sign up and get started!
+        </Header>
+      )}
     </Layout>
   );
 };
